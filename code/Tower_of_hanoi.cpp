ff6a43a02478ab9971bd43dedc2f35a6648b28ac
Khushi Rana <91715072+KhushiRana2003@users.noreply.github.com>
2021-10-02 14:46:56 +0530

    Create Tower_of_hanoi.cpp

---
+++ code/Tower_of_hanoi.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+
+void HanoiTower(int n, char src, char dest, char helper)
+{
+
+    if(n==0)    //base case
+    {
+        return;
+    }
+    HanoiTower(n-1,src,helper,dest);
+    cout<<"Mover from "<< src <<" to "<<dest<<endl;
+    HanoiTower(n-1,helper,dest,src);
+}
+
+int main()
+{
+    HanoiTower(3,'A','C','B');
+    return 0;
+}
